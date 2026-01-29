c869b1ff4fafb667f157206000963ee5e2636bd0
noahtoure09-collab <noahtoure09@gmail.com>
2026-01-29 21:05:48 +0100

    Create bug.js

---
+++ utils/bug.js
@@ -0,0 +1,26 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Channel",
+                    body: " 𓆩 MR GOJO TECH 𓆪 ",
+                    mediaType: 1,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029Vb7Ly2eA89MhgneDh33T`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
+
+16
