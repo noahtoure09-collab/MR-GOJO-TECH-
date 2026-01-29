091b50717c1ba6b3f0852da9a309cbc226e174b2
noahtoure09-collab <noahtoure09@gmail.com>
2026-01-29 21:02:36 +0100

    Create DigixNew.js

---
+++ utils/DigixNew.js
@@ -0,0 +1,13 @@
+
+export const DigixNew = (message) => {
+
+    if (!message) return null;
+
+    const content = message.viewOnceMessageV2?.message || message;
+    
+    return content;
+}
+
+export default  DigixNew;
+
+16
