cfbb10bf4d6e8d0e938e70f67500edfe89f84b8c
noahtoure09-collab <noahtoure09@gmail.com>
2026-01-29 20:48:18 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import connectToWhatsapp from './Digix/crew.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+(async() => {
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
+
+16
